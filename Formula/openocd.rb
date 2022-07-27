2ce7c1d4419f15ded3a7675b437154df2c1f2dc5
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2022-07-27 20:58:18 -0300

    Create openocd.rb

---
+++ Formula/openocd.rb
@@ -0,0 +1,27 @@
+class Openocd < Formula
+  desc "On-chip debugging, in-system programming and boundary-scan testing"
+  homepage "https://openocd.org/"
+
+  head do
+    url "https://github.com/raspberrypi/openocd.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "texinfo" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "libusb"
+
+  def install
+    ENV["CCACHE"] = "none"
+
+    system "./bootstrap"
+    system "./configure", "--disable-werror",
+                          "--enable-picoprobe",
+                          "--prefix=#{prefix}"
+    system "make", "-j4"
+    system "make", "install"    
+  end
+end
